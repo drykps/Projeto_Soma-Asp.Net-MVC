fa5df854e4a0ce68929505bafa0bf1646241aeb9
Adriana Penha <dry-k.ps@hotmail.com>
2020-01-14 21:51:26 -0300

    PRIMEIRO COMMIT

---
+++ Teste/Content/js/custom.js
@@ -0,0 +1,35 @@
+﻿var iniciarBotoes = function () {
+    var btnSomar = document.getElementById('btn-somar');
+
+    if (btnSomar) {
+        btnSomar.addEventListener('click', function () {
+            carregarResultado();
+        });
+    }
+};
+
+var carregarResultado = function () {
+    var data = $('#form-soma').serialize();
+    var numero1 = document.getElementById('numero1');
+    var numero2 = document.getElementById('numero2');
+
+
+
+    if ((numero1 && numero2 && (numero1.value && numero2.value)) && data) {
+        $.ajax({
+            url: "/Home/Total",
+            type: 'POST',
+            data: data,
+        }).done(function (res) {
+            alert("Resultado: " + res);
+        }).fail(function () {
+            alert("Erro");
+        });;
+
+    } else {
+        alert("Por favor, preencha os campos.");
+
+    }
+};
+
+iniciarBotoes();
---
+++ Teste/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Teste.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Inicio
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Soma de valores";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Total(int numero1, int numero2)
+        {
+            int soma = 0;
+            soma = numero1 + numero2;
+            
+            return Content(soma+"");
+        }
+
+    }
+}
